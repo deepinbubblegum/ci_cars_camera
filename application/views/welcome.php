b8ae1e835e8de6961e2b7ca0665272162ce24452
unknown <Admin@L2010003.motto.biz>
2020-10-31 14:12:06 +0700

    first commit

---
+++ application/views/welcome.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="favicon.ico" type="image/ico">
+    <title>SING IN</title>
+    <link rel="stylesheet" href="./public/assets/css/bootstrap.min.css">
+</head>
+
+<body>
+    <!-- javascript  -->
+    <script src="./public/assets/js/jquery-3.5.1.min.js"></script>
+    <script src="./public/assets/js/popper.min.js"></script>
+    <script src="./public/assets/js/bootstrap.js"></script>
+</body>
+
+</html>
